b7d8e15b416cd7817ea86c0a6c5ffe6c262a71e9
Yi <ymin@orange-vallee.net>
2015-04-15 15:25:11 +0200

    Initial Commit

---
+++ Notiforward/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Notiforward
+//
+//  Created by Sparky on 15/04/15.
+//  Copyright (c) 2015 minus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Notiforward/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Notiforward
+//
+//  Created by Sparky on 15/04/15.
+//  Copyright (c) 2015 minus. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
